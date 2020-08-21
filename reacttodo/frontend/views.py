b6eb010127e1c5446bf0213606fa11bc2a4ebd5e
yuara <yu.arakawa112@gmail.com>
2020-08-21 23:06:02 +0900

    Created detail view for todo in frontend/views.py

--- reacttodo/frontend/views.py
+++ reacttodo/frontend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from todos.models import Todo
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "frontend/index.html")
+
+
+class TodoDetailView(DetailView):
+    model = Todo
+    template_name = "frontend/index.html"
