5615c45dcc052e9737e8faaec55dca54bbf54f60
yuara <yu.arakawa112@gmail.com>
2020-08-22 17:07:12 +0900

    Configured routing using React Router

--- reacttodo/frontend/src/components/App.js
+++ reacttodo/frontend/src/components/App.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import Dashboard from './todos/Dashboard';
 import { Provider } from 'react-redux';
 import store from '../store';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from '../history';
 import Header from './layout/Header';
+import TodoDetele from './todos/TodoDelete';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
-        <Dashboard />
+        <Router history={history}>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path='/delete/:id' component={TodoDetele} />
+          </Switch>
+        </Router>
       </Provider>
     );
   }
