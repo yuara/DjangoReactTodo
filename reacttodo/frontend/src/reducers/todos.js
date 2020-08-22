d7c4e9d73cde6539731ff628f82b71ccf1f9625f
yuara <yu.arakawa112@gmail.com>
2020-08-22 16:32:18 +0900

    Added GET_TODO and DELETE_TODO in reducers

--- reacttodo/frontend/src/reducers/todos.js
+++ reacttodo/frontend/src/reducers/todos.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { GET_TODOS, ADD_TODO } from '../actions/types';
+import { GET_TODOS, GET_TODO, ADD_TODO, DELETE_TODO } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -8,11 +8,14 @@ export default (state = {}, action) => {
         ...state,
         ..._.mapKeys(action.payload, 'id')
       };
+    case GET_TODO:
     case ADD_TODO:
       return {
         ...state,
         [action.payload.id]: action.payload
       };
+    case DELETE_TODO:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
