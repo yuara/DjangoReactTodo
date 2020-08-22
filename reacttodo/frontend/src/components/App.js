57a9905f51fff0a2172cfe7d8f72bc8e8653c9d6
yuara <yu.arakawa112@gmail.com>
2020-08-22 16:21:19 +0900

    Added Header in App.js

--- reacttodo/frontend/src/components/App.js
+++ reacttodo/frontend/src/components/App.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import Dashboard from './todos/Dashboard';
 import { Provider } from 'react-redux';
 import store from '../store';
+import Header from './layout/Header';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <Header />
         <Dashboard />
       </Provider>
     );
