5d9d76d32d3ba848765cbb5c7648bc12ccf51d31
yuara <yu.arakawa112@gmail.com>
2020-08-22 15:40:32 +0900

    Updated app.js

--- reacttodo/frontend/src/components/App.js
+++ reacttodo/frontend/src/components/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import Dashboard from './todos/Dashboard';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>ToDoCRUD</h1>
-      </div>
+      <Provider store=(store)>
+        <Dashboard />
+      </Provider>
     );
   }
 }
-
-ReactDOM.render(<App />, document.querySelector('#app'));
