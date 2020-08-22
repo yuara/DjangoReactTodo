c5563c5f29dcd513d8b9c15e659e52d4c8de0f2e
yuara <yu.arakawa112@gmail.com>
2020-08-22 15:58:18 +0900

    Added ADD_TODO in reducers/todos

--- reacttodo/frontend/src/reducers/todos.js
+++ reacttodo/frontend/src/reducers/todos.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { GET_TODOS } from '../actions/types';
+import { GET_TODOS, ADD_TODO } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -7,6 +7,11 @@ export default (state = {}, action) => {
       return {
         ...state,
         ..._.mapKeys(action.payload, 'id')
+      };
+    case ADD_TODO:
+      return {
+        ...state,
+        [action.payload.id]: action.payload
       };
     default:
       return state;
